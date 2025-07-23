66e9a741179b96281d5eb34ccb30d90214246c9c
Martin Castillo <58961096+waskull@users.noreply.github.com>
2025-07-23 15:14:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:date", function (req, res) {
+  try {    
+    if (Number(req.params.date)) {
+      const unixDate = parseInt(req.params.date);
+      const milliseconds = unixDate * 1000;
+      const date = new Date(milliseconds);
+      const utc = date.toUTCString();
+      return res.json({ "unix": unixDate, "utc": utc });
+    }
+    const date = new Date(req.params.date);
+    const utc = date.toUTCString();
+    return res.json({ "unix": date.getTime(), "utc": utc });
+  } catch (e) {
+    return res.json({ error: "Invalid Date" });
+  }
+});
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
